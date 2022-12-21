45d6cffa35778723f32c51dd0c2e82aee4efa37c
JeremyEffinger <Jeremy.Effinger@gmail.com>
2022-12-21 04:40:39 -0600

    housekeeping

--- Public/app.js
+++ Public/app.js
@@ -1,3 +1,4 @@
+//Generate a unique UUID v4 format for a user.
 function uuidv4() {
   return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
     var r = (Math.random() * 16) | 0,
@@ -6,6 +7,7 @@ function uuidv4() {
   });
 }
 
+//Format a nice time for a message that is sent.
 function formatDate(date) {
   const h = "0" + date.getHours();
   const m = "0" + date.getMinutes();
@@ -13,19 +15,21 @@ function formatDate(date) {
   return `${h.slice(-2)}:${m.slice(-2)}`;
 }
 
+//Check to see if a session exists for the site
 if (!window.sessionStorage.chatterabi) {
-  let color = `#${Math.floor(Math.random() * 16777215).toString(16)};`;
-  let id = uuidv4();
-  window.sessionStorage.setItem("chatterabi", JSON.stringify({ id, color }));
+  let color = `#${Math.floor(Math.random() * 16777215).toString(16)};`; //assign a random hex color code for user
+  let id = uuidv4(); //assign uuid
+  window.sessionStorage.setItem("chatterabi", JSON.stringify({ id, color })); //store both id and color for session.
 }
 
-const { id, color } = JSON.parse(window.sessionStorage.getItem("chatterabi"));
+const { id, color } = JSON.parse(window.sessionStorage.getItem("chatterabi")); //Get user ID and color
 
-const socket = new WebSocket(`wss://${window.location.host}/`);
+const socket = new WebSocket(`wss://${window.location.host}/`); //create a new websocket connection window location (localhost for dev, or deployment location.)
 
 socket.addEventListener("open", (event) => {
   let $form = $(".msger-inputarea");
 
+  //On submit form event send message
   $form.on("submit", (event) => {
     let $input = $(".msger-input");
     var text = $input.val();
@@ -38,11 +42,12 @@ socket.addEventListener("open", (event) => {
     return false;
   });
 
+  //on message event render message in html.
   socket.onmessage = (msg) => {
     console.log(msg);
     let response = JSON.parse(msg.data);
     let $messageList = $(".msger-chat");
-    let side = response.id === id ? "left" : "right";
+    let side = response.id === id ? "left" : "right"; //If message sent by you right side, if not left side.
     $messageList.append(`<div class="msg ${side}-msg">
     <div class="msg-img" style="background-image: url(${"./img/anonymous.png"})"></div>
 
@@ -57,6 +62,6 @@ socket.addEventListener("open", (event) => {
       <div class="msg-text">${response.text}</div>
     </div>
   </div>`);
-    $messageList.animate({ scrollTop: $messageList.prop("scrollHeight") }, 500);
+    $messageList.animate({ scrollTop: $messageList.prop("scrollHeight") }, 500); //ensure chatwindow scrolls to bottom.
   };
 });
--- server.js
+++ server.js
@@ -5,23 +5,30 @@ import path from "path";
 import postgress from "postgres";
 import expressWs from "express-ws";
 
+//Setup express server
 const server = express();
 expressWs(server);
 const port = 3000;
 
+//SQL config
 const db_url = process.env.CHATTERABI_DB_URL;
 const sql = postgress(db_url);
-const __dirname = path.resolve();
-const clients = new Map();
 
+const __dirname = path.resolve(); //ensure we have access to __dirname for mapping files to static html.
+
+const clients = new Map(); //create map to hold clients
+
+//Setup middleware
 server.use(express.json());
 server.use(express.static("Public"));
 
+//Used for testing
 server.use(function (req, res, next) {
   req.testing = "setup";
   return next();
 });
 
+//Start of api routes
 server.get("/api/history", (req, res, next) => {
   sql`SELECT * FROM messages;`.then((history) => {
     res.json(history);
@@ -40,35 +47,43 @@ server.get("/api/history/user/:id", (req, res, next) => {
   });
 });
 
+//Static server history page
 server.get("/history", (req, res, next) => {
-  res.sendFile(`${__dirname}/Public/history.html`);
+  res.sendFile(`${__dirname}/Public/history.html`); //use __dirname to give entire path for file.
 });
 
+//Setup websocket connection on /index.html connection.
 server.ws("/", (ws, req) => {
   clients.set(ws);
   console.log("connection", req.testing);
 
+  //On message recieved event
   ws.on("message", function (msg) {
-    const { id, text } = JSON.parse(msg);
-    const now = new Date();
+    const { id, text } = JSON.parse(msg); //parse id and text from the message
+    const now = new Date(); //create a new date to pass to database
     sql`INSERT INTO messages (message, timesent, sender_id) VALUES (${text}, ${now}, ${id}) RETURNING *`.then(
-      (result) => console.log(result[0])
+      (result) => console.log(result[0]) //Commit message to db and log result.
     );
+    //Send each message to each client in the client map.
     [...clients.keys()].forEach((client) => {
       client.send(msg);
     });
   });
-  console.log("socket", req.testing);
+
+  console.log("socket", req.testing); //log out that the socket has been setup.
+});
+
+//fail over routes
+server.use((req, res, next) => {
+  res.contentType("text/plain").status(404).send("Not Found");
 });
 
 server.use((err, req, res, next) => {
   console.error(err);
   res.status(500).send("Internal Server Error");
 });
-server.use((req, res, next) => {
-  res.contentType("text/plain").status(404).send("Not Found");
-});
 
+//Run the express/ws server
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
